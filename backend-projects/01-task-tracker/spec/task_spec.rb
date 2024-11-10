d49c49f50200622acf9fb3f53d5ebc07807e50d1
Emmanuel Akachukwu <emmanuelakachukwu1@gmail.com>
2024-11-10 23:40:13 +0100

    test: add tests for add method

--- backend-projects/01-task-tracker/spec/task_spec.rb
+++ backend-projects/01-task-tracker/spec/task_spec.rb
@@ -4,14 +4,53 @@ require_relative '../lib/task'
 require 'json'
 
 RSpec.describe Task do
+  before(:all) do
+    @json_file = "#{__dir__}/../.tasks.json"
+  end
+
+  after(:all) do
+    File.delete(@json_file) if File.exist?(@json_file)
+  end
+
+  subject { described_class.new }
+
   describe '#initialize' do
     it 'sets up the datastore' do
-      expect(File).to exist("#{__dir__}/../.tasks.json")
+      subject
+      expect(File).to exist(@json_file)
     end
 
     it 'fetches tasks' do
-      task = Task.new
-      expect(task.send(:tasks)).to be_an_instance_of(Array)
+      expect(subject.send(:tasks)).to be_an_instance_of(Array)
     end
+  end
+
+  describe '#add' do
+    let!(:task_count) { subject.send(:tasks).count }
+
+    before do
+      subject.add(
+        'Say hello to the world',
+        description: "We're saying hello world"
+      )
+    end
+
+    it 'increments task list count by 1' do
+      expect(subject.send(:tasks).count).to eq(task_count + 1)
+    end
+
+    it 'adds the task details as specified' do
+      expect(
+        subject.send(:tasks).last.slice(:id, :task, :description, :status)).to eq(
+        id: task_count + 1,
+        task: 'Say hello to the world',
+        description: "We're saying hello world",
+        status: :todo
+      )
+    end
+  end
+
+  describe '#update' do
+    //
   end
 end
